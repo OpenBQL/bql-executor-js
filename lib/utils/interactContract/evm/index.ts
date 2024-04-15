16d43887e5b7ed386a1fbfaeca674438061e2045
huadee1108 <981929654@qq.com>
2024-04-15 17:38:43 +0800

    feat: add interact contract

--- lib/utils/interactContract/evm/index.ts
+++ lib/utils/interactContract/evm/index.ts
@@ -1,5 +1,6 @@
 import { getValueByPath } from "../../common";
 import Web3 from "web3";
+import * as ERC20 from "../../../abis/erc20.json";
 
 const interactContractEvm = async (
   key: string,
@@ -10,8 +11,8 @@ const interactContractEvm = async (
   const pathValue = getValueByPath(context, path);
   if (pathValue) {
     const action = pathValue[key];
+    const web3 = new Web3(provider);
     if (action.protocol === "NativeToken") {
-      const web3 = new Web3(provider);
       const transactionPromise = new Promise((resolve, reject) => {
         web3.eth
           .sendTransaction(action.params)
@@ -26,7 +27,33 @@ const interactContractEvm = async (
           });
       });
       await transactionPromise;
-      console.log("xxx");
+    } else {
+      let res;
+      const contractObj = await new web3.eth.Contract(
+        ERC20 as any,
+        action.contract
+      );
+      if (action.params) {
+        if (action.value && !isNaN(action.value) && action.value !== "0") {
+          res = await contractObj?.methods?.[action.call]?.(
+            ...Object.values(action.params),
+            { value: action.value }
+          ).call();
+        } else {
+          res = await contractObj?.methods?.[action.call]?.(
+            ...Object.values(action.params)
+          ).call();
+        }
+      } else {
+        if (action.value && !isNaN(action.value) && action.value !== "0") {
+          res = await contractObj?.methods?.[action.call]?.({
+            value: action.value,
+          }).call();
+        } else {
+          res = await contractObj?.methods?.[action.call]?.().call();
+        }
+      }
+      console.log(res);
     }
   }
 };
